6cf3b01d3c38fc65736420fed2675d4c983c3a9a
damephena <okoyeifenna24@gmail.com>
2020-05-24 22:50:28 +0100

    minor removal

--- services/models.py
+++ services/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db.models import Avg, Count, Min, Sum, Max
 from django.core.mail import send_mail
 from django.utils.translation import ugettext_lazy as _
 
